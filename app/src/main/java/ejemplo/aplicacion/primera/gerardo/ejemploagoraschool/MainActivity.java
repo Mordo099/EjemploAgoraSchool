63d74a956c8978ee83e0793bcd148274b156c3df
Mordo099 <gerardo_martinezh2@hotmail.com>
2018-06-13 13:12:20 -0600

    first commit

---
+++ app/src/main/java/ejemplo/aplicacion/primera/gerardo/ejemploagoraschool/MainActivity.java
@@ -0,0 +1,13 @@
+package ejemplo.aplicacion.primera.gerardo.ejemploagoraschool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
